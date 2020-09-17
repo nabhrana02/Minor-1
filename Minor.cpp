8c7540a49342b6aab038cc3ddb9ec81d82a7a7e2
Nabh Rana <nabhrana02@gmail.com>
2020-09-17 11:21:14 +0530

    Interface1
    Nabh

--- Minor.cpp
+++ Minor.cpp
@@ -3,19 +3,33 @@
 
 void Interface1() {
 	
+	//Interface 1 Design Display.
+	// Om PAndey push your function here.*(Only the display function).
+
+	
 }
 
 void display() {
 	Interface1();
+
+
 }
+
+
+
 void reshaping(int w, int h) {
 	glViewport(0, 0, w, h);
 	gluOrtho2D(0, 1080, 0, 720);
 
 }
+
+
+
 void init() {
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 }
+
+
 
 
 int main(int argc, char** argv) {
