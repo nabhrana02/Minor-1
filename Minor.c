551da346d3fa5d49ac63084c9b3595390c883f1a
shashankpanthri <benkyoallthetime@gmail.com>
2020-09-19 21:03:52 +0530

    made some changes to the display option function

--- Minor.c
+++ Minor.c
@@ -37,19 +37,19 @@ void mouse(int button, int state, int x, int y)
 
 //Output based on User's Input.
 //Shashank push your code here.
-void display_option(int selected1[], int selected2[]){
+void display_option(int Selected1[], int Selected2[]){
 
     glColor3f(1.0,1.0,1.0);
     glBegin(GL_POLYGON);
-    glVertex2f(((selected1[2]-selected1[0])/2)-12, selected1[3]+15);
-    glVertex2f(((selected1[2]-selected1[0])/2)+12, selected1[3]+15);
-    glVertex2f((selected1[2]-selected1[0])/2, selected1[3]+5);
+    glVertex2f((((Selected1[2]-Selected1[0])/2)-14)+Selected1[0], Selected1[3]+17);
+    glVertex2f((((Selected1[2]-Selected1[0])/2)+14)+Selected1[0], Selected1[3]+17);
+    glVertex2f(((Selected1[2]-Selected1[0])/2)+Selected1[0], Selected1[3]+5);
     glEnd();
 
     glBegin(GL_Polygon);
-    glVertex2f(((selected2[2]-selected2[0])/2)-12, selected2[3]+15);
-    glVertex2f(((selected2[2]-selected2[0])/2)+12, selected2[3]+15);
-    glVertex2f((selected2[2]-selected2[0])/2, selected2[3]+5);
+    glVertex2f((((Selected2[2]-Selected2[0])/2)-14)+Selected2[0], Selected2[3]+17);
+    glVertex2f((((Selected2[2]-Selected2[0])/2)+14)+Selected2[0], Selected2[3]+17);
+    glVertex2f(((Selected2[2]-Selected2[0])/2)+Selected2[0], Selected2[3]+5);
     glEnd();
 
 }
